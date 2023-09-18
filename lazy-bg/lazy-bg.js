362e0aa8497e42ebb9c162ba2db4e58feaaa71c7
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-09-18 14:38:44 +1000

    enhanced function enabling multiple elements

--- lazy-bg/lazy-bg.js
+++ lazy-bg/lazy-bg.js
@@ -1,24 +1,33 @@
 function lazyLoadImages() {
-    const watchInActionDiv = document.querySelector('.bg-lazy');
-    const isMobile = window.innerWidth < 768;
-    if (isMobile) {
-    console.log('isMobile is true')
-    }else{
-      console.log('isMobile is false')
-    }
+  const lazyElements = document.querySelectorAll('.bg-responsive-lazy');
+  const isMobile = window.innerWidth < 768;
+
+  if (isMobile) {
+    console.log('isMobile is true');
+  } else {
+    console.log('isMobile is false');
+  }
+
+  lazyElements.forEach((watchInActionDiv) => {
     let imageUrl = isMobile ? watchInActionDiv.getAttribute('data-mobile-image') : watchInActionDiv.getAttribute('data-pc-image');
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const lazyImage = entry.target;
-          if (imageUrl) {
+
+    if (imageUrl) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const lazyImage = entry.target;
+
             lazyImage.style.backgroundImage = `url(${imageUrl})`;
             imageUrl = null;
+
+            observer.unobserve(lazyImage);
           }
-          observer.unobserve(lazyImage);
-        }
+        });
       });
-    });
-    observer.observe(watchInActionDiv);
-  }
-  window.addEventListener('load', lazyLoadImages);
+
+      observer.observe(watchInActionDiv);
+    }
+  });
+}
+
+window.addEventListener('load', lazyLoadImages);
